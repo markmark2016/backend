32ec79ae9760e96b2f78bcc853339417f55ddd84
tianxiao545 <305409418@qq.com>
2016-03-12 21:22:19 +0800

    a

--- project/mark-backend/src/main/java/com/mark/backend/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.mark.backend.service;
-
-import com.mark.backend.mysql.po.User;
-
-public interface IUserService {
-	User getUserByOpenId(String openId);
-
-	int insertUser(User user);
-}
