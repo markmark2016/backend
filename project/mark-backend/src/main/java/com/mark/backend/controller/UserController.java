d537458de1109ecefb87d2c722a85986a82ea256
tianxiao545 <305409418@qq.com>
2016-03-12 20:34:47 +0800

    返回json数据

--- project/mark-backend/src/main/java/com/mark/backend/controller/UserController.java
+++ project/mark-backend/src/main/java/com/mark/backend/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.mark.backend.controller;
 
-import java.io.IOException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.mark.backend.mysql.po.User;
 import com.mark.backend.service.IUserService;
 
@@ -24,10 +22,8 @@ public class UserController {
 	@RequestMapping(value = "/info/get/{openId}", method = RequestMethod.GET)
 	public @ResponseBody
 	Object getUserInfo(@PathVariable("openId") String openId,
-			HttpServletResponse response) throws IOException {
+			HttpServletResponse response) {
 		User user = userService.getUserByOpenId(openId);
-		System.out.println(JSON.toJSON(user));
-		// response.getWriter().print(JSON.toJSON(user));
 		return user;
 	}
 }
