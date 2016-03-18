7d1950ed370e31f9e4c6602f31e2b3ace8af8514
tianxiao545 <305409418@qq.com>
2016-03-18 10:09:28 +0800

    后台用户登录功能

--- src/main/java/com/mark/backend/controller/AdminController.java
+++ src/main/java/com/mark/backend/controller/AdminController.java
@@ -5,11 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mark.backend.model.Admin;
+
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(Admin admin, Model model) {
+		if (admin.getUsername() == "imark" && admin.getPassword() == "root")
+			return "admin/index";
+		return "redirect:/admin";
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(Model model) {
-		return null;
+		return "admin/index";
 	}
 }
