289156dcb1c75bd1d2e541c274c8cb6db96c6cb4
tianxiao545 <305409418@qq.com>
2016-03-23 15:06:17 +0800

    增加message数据库操作相关文件

---
+++ src/main/java/com/mark/backend/mysql/po/userMessage.java
@@ -0,0 +1,95 @@
+package com.mark.backend.mysql.po;
+
+import java.util.Date;
+
+public class userMessage {
+    private Long id;
+
+    private Long userIdFk;
+
+    private String content;
+
+    private Long interactIdFk;
+
+    private String type;
+
+    private String url;
+
+    private String isCheck;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserIdFk() {
+        return userIdFk;
+    }
+
+    public void setUserIdFk(Long userIdFk) {
+        this.userIdFk = userIdFk;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Long getInteractIdFk() {
+        return interactIdFk;
+    }
+
+    public void setInteractIdFk(Long interactIdFk) {
+        this.interactIdFk = interactIdFk;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public String getIsCheck() {
+        return isCheck;
+    }
+
+    public void setIsCheck(String isCheck) {
+        this.isCheck = isCheck == null ? null : isCheck.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
