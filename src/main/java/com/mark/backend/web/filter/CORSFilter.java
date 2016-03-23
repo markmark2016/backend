5cdb3bd911c750c5ad39ca890f77e73f1fd24a21
tianxiao545 <305409418@qq.com>
2016-03-23 19:27:31 +0800

    解决跨域资源冲突问题

---
+++ src/main/java/com/mark/backend/web/filter/CORSFilter.java
@@ -0,0 +1,47 @@
+package com.mark.backend.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * 解决跨域资源冲突问题
+ * @Title:CORSFilter
+ * @Description:TODO
+ * @author YangTianxiao
+ * @date 2016年3月23日
+ *
+ */
+public class CORSFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods",
+				"POST, GET, OPTIONS, DELETE,PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		chain.doFilter(req, res);
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
